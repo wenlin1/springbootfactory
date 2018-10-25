ba01238844049d7bbaf51d3130a51304ada0520e
519628wenlin <wenlin>
2018-10-25 15:41:25 +0800

    单例模式添加注释

--- src/main/java/cn/wl/singleton/Singleton5.java
+++ src/main/java/cn/wl/singleton/Singleton5.java
@@ -8,6 +8,13 @@ package cn.wl.singleton;
  * @CreateDate: 2018-10-24
  */
 public class Singleton5 {
+
+    /**
+     * 内部类的加载：内部类（不论是静态内部类还是非静态内部类）都是在第一次使用时才会被加载。
+     * 外部类不调用 getInstance（）时候 内部类是不会加载的，所以达到了懒汉的效果； 然后调用的时候 内部类被加载，
+     * 加载的时候就会初始化实例；这个加载的过程是不会有多线程的问题的！
+     * 类加载的时候有一种机制叫做 缓存机制；第一次加载成功之后会被缓存起来；而且一般一个类不会加载多次
+     */
     private static class SingletonHolder{
         private static final Singleton5 instance = new Singleton5();
     }
